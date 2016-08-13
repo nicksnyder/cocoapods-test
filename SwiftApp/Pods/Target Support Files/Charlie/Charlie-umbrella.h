8911c2e5061cdb01ec0d640d241bbd41f7c02730
Nick Snyder <nsnyder@linkedin.com>
2016-08-12 19:58:25 -0700

    add dependencies

---
+++ SwiftApp/Pods/Target Support Files/Charlie/Charlie-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "Charlie.h"
+#import "CharlieProvider.h"
+
+FOUNDATION_EXPORT double CharlieVersionNumber;
+FOUNDATION_EXPORT const unsigned char CharlieVersionString[];
+
---
+++ SwiftApp/Pods/Target Support Files/Pods-SwiftAppUITests/Pods-SwiftAppUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SwiftAppUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SwiftAppUITestsVersionString[];
+
